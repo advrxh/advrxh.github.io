9395f3c77870fec203676feba137bf48cf0212d6
Aadil Varsh <aadilvarshofficial@gmail.com>
2022-01-22 16:25:18 +0530

    Update findme styles

--- src/styles/compStyles/findmePage.style.tsx
+++ src/styles/compStyles/findmePage.style.tsx
@@ -3,25 +3,25 @@
 import { createStyles, makeStyles, Theme } from "@material-ui/core";
 
 // LOCAL IMPORTS
-
 // CONSTANTS
 
 // MAIN
 
-
-const FindMePageStyle = makeStyles((theme: Theme) => createStyles({
-    PageCont: {
-        width: '100vw',
-        height: '100vh',
-        background: theme.palette.secondary.main
-    },
-    icon: {
-        fontSize: theme.typography.fontSize + 40
-    },
-    routeCont: {
-        width: 'auto',
-        height: 'auto'
-    }
-}))
+const FindMePageStyle = makeStyles((theme: Theme) =>
+    createStyles({
+        PageCont: {
+            width: "100vw",
+            height: "100vh",
+            background: theme.palette.secondary.main,
+        },
+        icon: {
+            fontSize: theme.typography.fontSize + 40,
+        },
+        routeCont: {
+            width: "auto",
+            height: "auto",
+        },
+    })
+);
 
 export default FindMePageStyle;
